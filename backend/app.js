4b9243a9e59bfb793019862cfa7882efacfc7d11
Sergii <s.kirianov@gmail.com>
2021-03-04 01:39:04 +0200

    Made backend work. MongoDB connected. GET and POST checked

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+  .then(() => {
+    logger.info('connected to Mongo DB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to Mongo DB', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('build'));
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
+
+module.exports = app;
